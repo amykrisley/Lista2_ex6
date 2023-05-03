7e5e0d843c50f2a60a6f86bd905e60193cda28f4
amykrisley <130614797+amykrisley@users.noreply.github.com>
2023-05-03 18:16:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2_ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, p, r;
+
+            Console.Write("Qual é o seu peso(em kilos): ");
+            p = double.Parse(Console.ReadLine());
+
+            Console.Write("Qual é a sua altura(em metros): ");
+            h = double.Parse(Console.ReadLine());
+
+            r = p / Math.Pow(h, 2);
+
+            if (r < 20)
+                Console.WriteLine("Abaixo do peso");
+            else
+                if (r < 25)
+                Console.WriteLine("Peso ideal");
+            else
+                Console.WriteLine("Acima do peso");
+        }
+    }
+}
